c480e72eb511409792ea2a5b5bb7557cd432b9a2
yashika532 <jainyashika532@gmail.com>
2024-06-11 01:07:30 +0530

    Gemini clone completed

---
+++ src/config/gemini.js
@@ -0,0 +1,48 @@
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ *
+ * See the getting started guide for more information
+ * https://ai.google.dev/gemini-api/docs/get-started/node
+ */
+
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+
+
+const genAI = new GoogleGenerativeAI("AIzaSyCXdEtYqZ2CSq-yrIDyldyhhkC7yWDO5NU");
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [
+    ],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  const response = result.response;
+  console.log(response.text());
+  return response.text();
+}
+
+export default run;
+
+
+// const API_KEY = "AIzaSyCXdEtYqZ2CSq-yrIDyldyhhkC7yWDO5NU";
+
